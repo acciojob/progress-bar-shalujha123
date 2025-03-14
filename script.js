7c41fc39d4ba0d82cbbbd9c7d05aa7ae7eabf1ae
Shalu <88194182+shalujha123@users.noreply.github.com>
2025-03-14 18:11:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
 //your JS code here. If required.
+let circles = document.querySelectorAll(".circle")
+let progressBar = document.getElementById("progress-bar")
+let prev = document.getElementById("prev")
+let next = document.getElementById("next")
+let currentState = 1
+
+next.addEventListener("click",()=>{
+	currentState++
+	updateProgress()
+})
+
+prev.addEventListener("click",()=>{
+	currentState--
+	updateProgress()
+})
+
+function updateProgress(){
+	circles.forEach((circle,index)=>{
+		if(index < currentState){
+			circle.classList.add("active")
+		}else{
+			circle.classList.remove("active")
+		}
+	})
+	progressBar.style.width = ((currentState-1)/(circles.length-1)) * 100 + "%"
+	prev.disabled = currentState === 1
+	next.disabled = currentState === circles.length
+}
+
+
+
+
+
+
